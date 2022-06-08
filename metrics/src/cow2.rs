2d1f60d44acbf6ff12c4601e1866cb901a8d06a9
Toby Lawrence <toby@nuclearfurnace.com>
2022-06-08 09:28:30 -0400

    more tweaks, docs, etc
    Signed-off-by: Toby Lawrence <toby@nuclearfurnace.com>

--- metrics/src/cow2.rs
+++ metrics/src/cow2.rs
@@ -1,13 +1,3 @@
-// TODO: We utilize the capacity field in `Metadata` as the sentinel value for "we-re holding an Arc<T>" but this
-// technically depends on the fact that Rust says it will not issue an allocation larger than `isize::MAX`, except maybe
-// on 32-bit systems? even then, it would still mean needing to get a string that was 2GB or larger which is just
-// like... not going to happen in real-life.
-//
-// _however_, we should be correct where we can be, so we should add a check to `from_owned` to enforce having a
-// capacity that isn't equal to `usize::MAX`. that's enough to ensure that our sentinel value is viable, and tbh, i
-// don't care about panicking if some weirdo has a string with enough reserved capacity to allocate usize::MAX bytes on
-// their platform. that ain't my problem.
-
 use std::{
     marker::PhantomData,
     mem::ManuallyDrop,
@@ -29,26 +19,125 @@ enum Kind {
     Shared,
 }
 
-/// A clone-on-write smart pointer with an optimized memory layout.
+/// A clone-on-write smart pointer with an optimized memory layout, based on `beef`.
 ///
-// TODO: Can we actually do a union of the normal pointer/metadata internals and a straight 24 byte array, used to hold
-// a string? Basically a clone of bodil's `smartstring`, since we can entirely avoid an allocation when using owned
-// values, preferring to copy them instead.
-//
-// Biggest problem is that with `NonNull<u8>`, it will always be non-zero. If we used an `Option<NonNull<u8>>`, that
-// gets us 15 bytes of storage: we lose 8 of our 24 needing `Option<NonNull<u8>>` to be `None` to indicate that the
-// string is inlined, and then we lose 1 byte to needing something to store the string length in.
-//
-// `smartstring` emulates `String` by allocating storage itself with a minimum alignment so that the pointer can have
-// the marker bit stored in the unused bit(s), and it does its own form of unioning by pointer casting between the
-// identically-sized boxed (ptr/len/cap) and inline (marker byte + u8 array) types.
-//
-// long story short, unless we want to reallocate every owned string to ensure a pointer aligned enough to also
-// store a marker bit, then we're stuck with tricks to take advantage of our existing pointer, and the only option we
-// have there is `Option<NonNull<u8>>`, i think.
-//
-// basically, 15 byte and 7 byte inlined strings on 64-bit and 32-bit platforms, respectively. 15 bytes is _probably_
-// good enough for a majority of label values, but 7 would be questionably useful.
+/// # Strings, strings everywhere
+///
+/// In `metrics`, strings are arguably the most common data type used despite fundamentally dealing with numerical
+/// measurements. Both the name of a metric, and its labels, are compromised a strings: emitting a metric may involve
+/// one string, or 10 strings. Even further, many of these strings tend to be used over and over during the life of the
+/// process.
+///
+/// In over to provide high performance, and to help avoid making constant, small allocations, using a "clone-on-write"
+/// smart pointer allows us to represent a logical data type -- such as a string -- in multiple forms, depending on
+/// which is available, and allows for seamless access to that data, whether its immutable access of creating an owned version.
+///
+/// # Why not `std::borrow::Cow`?
+///
+/// Typically, those writing Rust may reach for `std::borrow::Cow`, which already works well in many cases. However,
+/// `metrics` strives to provide minimal overhead where possible, and so `std::borrow::Cow` falls down in one particular
+/// way: it uses an enum representation which consumes an additional word of storage.
+///
+/// As an example, let's look at strings. A string in `std::borrow::Cow` implies that `T` is `str`, and the owned
+/// version of `str` is simply `String`. Thus, for `std::borrow::Cow`, the in-memory layout looks like this:
+///
+/// ```text
+///                                                            Padding
+///                                                               |
+///                                                               v
+///                       +--------------+-------------+--------------+--------------+
+/// stdlib Cow::Borrowed: |   Enum Tag   |   Pointer   |    Length    |   XXXXXXXX   |
+///                       +--------------+-------------+--------------+--------------+
+///                       +--------------+-------------+--------------+--------------+
+/// stdlib Cow::Owned:    |   Enum Tag   |   Pointer   |    Length    |   Capacity   |
+///                       +--------------+-------------+--------------+--------------+
+/// ```
+///
+/// Just as `beef` does, we encode the notion of borrowed vs owned into the the length and capacity fields directly,
+/// and use a struct, which avoids needing an implicit or explicit discriminant field:
+///
+/// ```text
+///                         +-------------+--------------+----------------+
+/// metrics Cow (borrowed): |   Pointer   |  Length (N)  |  Capacity (0)  |
+///                         +-------------+--------------+----------------+
+///                         +-------------+--------------+----------------+
+/// stdlib Cow (owned):     |   Pointer   |  Length (N)  |  Capacity (N)  |
+///                         +-------------+--------------+----------------+
+/// ```
+///
+/// # Why not `beef`?
+///
+/// Up until this point, it might not be clear why we didn't just use `beef`. And indeed, we've already admitted that
+/// this design is fundamentally based on `beef`. Crucially, however, `beef` did not/still does not support `const`
+/// construction for generic slices. Remember how we mentioned labels? The labels of a metric `are `[Label]`
+/// under-the-hood, and so without a way to construct them in a `const` fashion, our previous work to allow entirely
+/// static keys would not be possible.
+///
+/// Thus, we forked `beef` and copied into directly into `metrics` so that we could write a specialized `const`
+/// constructor for `[Label]`.
+///
+/// # What we do that `beef` doesn't do
+///
+/// It was already enough to use our own implementation for the specialized `const` capabilities, but we've taken things
+/// even further in two key ways: support for `Arc`-wrapped values, and inlined strings.
+///
+/// ## `Arc`-wrapped values
+///
+/// For many strings, there is still a desire to share them cheaply even when they are constructed at run-time.
+/// Remember, cloning a `Cow` of an owned value means cloning the value itself, so we need another level of indirection
+/// to allow the cheap sharing, which is where `Arc<T>` comes in.
+///
+/// Users can construct a `Arc<T>`, whether `T` is lines up with the `T` of `metrics::Cow2`, and use that as the initial
+/// value instead. When `Cow2` is cloned, we instead cloning the underlying `Arc<T>`, avoiding a new allocation.
+/// `Arc<T>` still handles all of the normal logic necessary to know when the wrapped value must be dropped, and how
+/// many live references to the value that there are, and so on.
+///
+/// We handle this by relying on an invariant of `Vec<T>`: it never allocates more than `isize::MAX` [1]. This lets us
+/// derive the following truth table of the valid combinations of length/capacity:
+///
+/// ```text
+///                           Length         Capacity
+///                     +---------------+----------------+
+/// Borrowed (&T):      |       N       |        0       |
+///                     +---------------+----------------+
+/// Owned (T::ToOwned): |       N       | M < usize::MAX |
+///                     +---------------+----------------+
+/// Shared (Arc<T>):    |       N       |   usize::MAX   |
+///                     +---------------+----------------+
+/// ```
+///
+/// As we only implement `Cow2` for types where their owned variants are either explicitly or implicitly backed by
+/// `Vec<_>`, we know that our capacity will never be `usize::MAX`, as it is limited to `isize::MAX`, and thus we can
+/// safely encode our "shared" state within the capacity field.
+///
+/// ## Inlined strings
+///
+/// As `Cow2` is primarily meant for string data, we never want to leave an optimization on the table, and another trick
+/// we added is string inlining.
+///
+/// String inlining is based on the concept of reinterpreting the memory used for a type to use it for raw string
+/// storage in certain cases.
+///
+/// Fundamentally, `Cow2` has a specific size of three words, or three `usize` values: 24 bytes on a 64-bit platform,
+/// for example. Instead of looking at `Cow2` as three `usize`-sized fields, what if we looked at it as a single `[u8;
+/// 24]` field? We could then store the raw bytes of a string that was short enough directly in the memory for `Cow2`.
+/// This lets us avoid pointer chasing when getting a reference to the string through `Cow2`, and it lets us avoid
+/// having to make a new allocation when cloning an owned version of `Cow2`.
+///
+/// Unfortunately, we can't just blindly store a 24-byte string inside `Cow2`, as we need some of those bytes to act as
+/// our discriminant -- is this an inlined string or not? -- as well as to hold the length of the string, so we can
+/// properly reconstruct the fundamental `&[u8]` that backs the `str`. This means we're limited to inlining strings up
+/// to a length of `2*N-1`: our normal pointer field becomes optional (its discriminant is our now our
+/// inlined/non-inlined discriminant) which means we lose a word, but since there is no known platform that Rust
+/// supports where two words would be over 255 bytes total, we can use a single `u8` to hold our string length.
+///
+/// Practically, this means strings up to 15 bytes can be inlined on 64-bit platforms, while 32-bit platforms are
+/// limited to 7 bytes.
+///
+/// # Notes
+///
+/// [1] - technically, `Vec<T>` can have a capacity greater than `isize::MAX` when storing zero-sized types, but we
+/// don't do that here, so we always enforce that an owned version's capacity cannot be `usize::MAX` when constructing `Cow2`
 pub struct Cow2<'a, T: Cow2able + ?Sized + 'a> {
     /// Pointer to data.
     ptr: Option<NonNull<T::Pointer>>,
@@ -60,6 +149,10 @@ pub struct Cow2<'a, T: Cow2able + ?Sized + 'a> {
     marker: PhantomData<&'a T>,
 }
 
+/// Smart pointer state.
+///
+/// Used to differentiate both whether or not the smart pointer is inlining a value, but also the specific data of each
+/// discriminant i.e. the data for an inlined value vs the length/capacity of a non-inlined value.
 #[derive(Clone, Copy)]
 pub union Inner {
     non_inlined: NonInlined,
@@ -75,6 +168,7 @@ impl Inner {
         Self { inlined: Inlined { length, data } }
     }
 }
+
 #[repr(transparent)]
 #[derive(Clone, Copy)]
 struct NonInlined(Metadata);
@@ -97,23 +191,31 @@ where
         Self { ptr: Some(ptr), inner: Inner::non_inlined(metadata), marker: PhantomData }
     }
 
+    /// Creates a smart pointer around an owned value.
     pub fn from_owned(owned: T::Owned) -> Self {
         let (ptr, metadata) = T::owned_into_parts(owned);
 
+        // This check is partially to guard against the semantics of `Vec<T>` changing in the future, and partially to
+        // ensure that we don't somehow implement `Cow2able` for a type where its owned version is backed by a vector of
+        // ZSTs, where the capacity could _legitimately_ be `usize::MAX`.
         if metadata.capacity() == usize::MAX {
-            panic!("invalid capacity of usize::MAX for owned value");
+            panic!("Invalid capacity of `usize::MAX` for owned value.");
         }
 
         Self::non_inlined(ptr, metadata)
     }
 
+    /// Creates a smart pointer around a shared value.
     pub fn from_shared(arc: Arc<T>) -> Self {
         let (ptr, metadata) = T::shared_into_parts(arc);
 
         Self::non_inlined(ptr, metadata)
     }
 
-    pub fn into_owned(self) -> T::Owned {
+    /// Extracts the owned data.
+    ///
+    /// Clones the data if it is not already owned.
+    pub fn into_owned(self) -> <T as ToOwned>::Owned {
         // We have to ensure that the `Drop` impl does _not_ run because we're taking ownership back in the case of
         // holding an owned value. `owned_from_parts` handles any necessary dropping that must occur i.e. if we're holding
         // an `Arc<T>`.
@@ -127,6 +229,7 @@ impl<'a, T> Cow2<'a, T>
 where
     T: Cow2able + ?Sized,
 {
+    /// Creates a smart pointer around a borrowed value.
     pub fn from_borrowed(borrowed: &'a T) -> Self {
         let (ptr, metadata) = T::borrowed_into_parts(borrowed);
 
@@ -230,29 +333,34 @@ impl Metadata {
         }
     }
 
-    pub const fn arc(length: usize) -> Metadata {
+    #[inline]
+    const fn shared(length: usize) -> Metadata {
         Metadata(length, usize::MAX)
     }
 
-    pub const fn borrowed(len: usize) -> Metadata {
+    #[inline]
+    const fn borrowed(len: usize) -> Metadata {
         Metadata(len, 0)
     }
 
-    pub const fn owned(len: usize, capacity: usize) -> Metadata {
+    #[inline]
+    const fn owned(len: usize, capacity: usize) -> Metadata {
         Metadata(len, capacity)
     }
 }
 
-pub trait Cow2able {
+pub trait Cow2able: ToOwned {
     type Pointer;
-    type Owned;
 
     fn borrowed_into_parts(&self) -> (NonNull<Self::Pointer>, Metadata);
-    fn owned_into_parts(owned: Self::Owned) -> (NonNull<Self::Pointer>, Metadata);
+    fn owned_into_parts(owned: <Self as ToOwned>::Owned) -> (NonNull<Self::Pointer>, Metadata);
     fn shared_into_parts(arc: Arc<Self>) -> (NonNull<Self::Pointer>, Metadata);
 
     fn borrowed_from_parts(ptr: Option<NonNull<Self::Pointer>>, inner: &Inner) -> *const Self;
-    fn owned_from_parts(ptr: Option<NonNull<Self::Pointer>>, inner: &Inner) -> Self::Owned;
+    fn owned_from_parts(
+        ptr: Option<NonNull<Self::Pointer>>,
+        inner: &Inner,
+    ) -> <Self as ToOwned>::Owned;
     fn clone_from_parts(
         ptr: Option<NonNull<Self::Pointer>>,
         inner: &Inner,
@@ -262,7 +370,6 @@ pub trait Cow2able {
 
 impl Cow2able for str {
     type Pointer = u8;
-    type Owned = String;
 
     #[inline]
     fn borrowed_into_parts(&self) -> (NonNull<Self::Pointer>, Metadata) {
@@ -283,7 +390,7 @@ impl Cow2able for str {
 
     #[inline]
     fn shared_into_parts(arc: Arc<Self>) -> (NonNull<Self::Pointer>, Metadata) {
-        let metadata = Metadata::arc(arc.len());
+        let metadata = Metadata::shared(arc.len());
         let ptr = unsafe { NonNull::new_unchecked(Arc::into_raw(arc) as *mut _) };
         (ptr, metadata)
     }
@@ -305,7 +412,10 @@ impl Cow2able for str {
     }
 
     #[inline]
-    fn owned_from_parts(ptr: Option<NonNull<Self::Pointer>>, inner: &Inner) -> Self::Owned {
+    fn owned_from_parts(
+        ptr: Option<NonNull<Self::Pointer>>,
+        inner: &Inner,
+    ) -> <Self as ToOwned>::Owned {
         // When the pointer is `None`, we're inlining the string. Otherwise, it's not inlined.
         match ptr {
             None => {
@@ -314,6 +424,7 @@ impl Cow2able for str {
                 let s = unsafe { &*Self::borrowed_from_parts(ptr, inner) };
                 s.to_string()
             }
+
             Some(ptr) => {
                 let metadata = unsafe { inner.non_inlined.0 };
                 match metadata.kind() {
@@ -323,12 +434,14 @@ impl Cow2able for str {
                         let s = unsafe { &*Self::borrowed_from_parts(Some(ptr), inner) };
                         s.to_string()
                     }
+
                     // We're reconstituting the `String` directly from the backing storage, rather than first reconstituing the
                     // `Vec<u8>` and then using the safe methods on `String`, because, well, we already know we got a valid
                     // `String` originally.
                     Kind::Owned => unsafe {
                         String::from_raw_parts(ptr.as_ptr(), metadata.length(), metadata.capacity())
                     },
+
                     // We specifically do _not_ forget the `Arc<str>` here, like we do in `clone_from_parts`.
                     //
                     // This method overall is called when `Cow2` is being consumed.  In the case of `Cow2` owning the string,
@@ -438,7 +551,6 @@ where
     T: Clone,
 {
     type Pointer = T;
-    type Owned = Vec<T>;
 
     #[inline]
     fn borrowed_into_parts(&self) -> (NonNull<Self::Pointer>, Metadata) {
@@ -448,7 +560,7 @@ where
     }
 
     #[inline]
-    fn owned_into_parts(owned: Self::Owned) -> (NonNull<Self::Pointer>, Metadata) {
+    fn owned_into_parts(owned: <Self as ToOwned>::Owned) -> (NonNull<Self::Pointer>, Metadata) {
         let mut owned = ManuallyDrop::new(owned);
         let ptr = unsafe { NonNull::new_unchecked(owned.as_mut_ptr()) };
         let metadata = Metadata::owned(owned.len(), owned.capacity());
@@ -457,7 +569,7 @@ where
 
     #[inline]
     fn shared_into_parts(arc: Arc<Self>) -> (NonNull<Self::Pointer>, Metadata) {
-        let metadata = Metadata::arc(arc.len());
+        let metadata = Metadata::shared(arc.len());
         let ptr = unsafe { NonNull::new_unchecked(Arc::into_raw(arc) as *mut _) };
         (ptr, metadata)
     }
@@ -474,7 +586,10 @@ where
     }
 
     #[inline]
-    fn owned_from_parts(ptr: Option<NonNull<Self::Pointer>>, inner: &Inner) -> Self::Owned {
+    fn owned_from_parts(
+        ptr: Option<NonNull<Self::Pointer>>,
+        inner: &Inner,
+    ) -> <Self as ToOwned>::Owned {
         // SAFETY: Only `str` can ever be inlined, not `[str]`, so we would panic on unwrapping `ptr` before
         // accessing `inner`, and so we cannot perform an invalid access of `inner`: if `ptr` is `Some(...)`, then
         // `self.non_inlined` must be initialized and valid.
